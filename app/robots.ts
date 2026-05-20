ae0224643b135668444e9f40a44d6a383aa1ffdc
knizhkoviysvit-commits <knizhkoviysvit@gmail.com>
2026-05-20 14:49:52 +0300

    Use primary sitemap in robots

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/"
     },
-    sitemap: "https://lawyer-kanivets.vercel.app/google-sitemap.xml"
+    sitemap: "https://lawyer-kanivets.vercel.app/sitemap.xml"
   };
 }
