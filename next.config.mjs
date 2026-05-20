506c6b8336d7ad0468b6ac28c7bc81a06e1ee9f7
knizhkoviysvit-commits <knizhkoviysvit@gmail.com>
2026-05-20 12:43:32 +0300

    Allow local dev origin

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  allowedDevOrigins: ["127.0.0.1"]
 };
 
 export default nextConfig;
